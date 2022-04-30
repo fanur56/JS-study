10ffef692d0846233bad32165abdf92d7c6065f9
fanur56 <ilasovf@gmail.com>
2022-04-30 20:49:34 +0300

    added new task/solution

--- Solutions.js
+++ Solutions.js
@@ -33,4 +33,14 @@ class SmallestIntegerFinder {
 }
 
 //=============================================================================
+/*
+This code does not execute properly. Try to figure out why.
+function multiply(a, b){
+  a * b
+}
+ */
+function multiply(a, b){
+    return(+a * +b);
+}
 
+//=================================================================================
