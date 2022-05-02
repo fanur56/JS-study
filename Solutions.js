d8c4e6f8948d07bd22483018ea9632a0187c4ab0
fanur56 <ilasovf@gmail.com>
2022-05-02 12:22:45 +0300

    added new task/solution

--- Solutions.js
+++ Solutions.js
@@ -88,3 +88,12 @@ function positiveSum(arr) {
         return sum + elem;
     }, 0)
 }
+
+//=======================================================================================
+/*
+Task
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+function even_or_odd(number) {
+    return (number % 2 === 0) ? "Even" : "Odd";
+}
