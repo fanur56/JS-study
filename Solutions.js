8b994b5357aa0b3c01d1591ce7c4d20213168775
fanur56 <ilasovf@gmail.com>
2022-05-07 18:52:24 +0300

    Add new solution

--- Solutions.js
+++ Solutions.js
@@ -141,3 +141,12 @@ function solution(str){
 }
 
 //==================================================================================
+/*
+Task:
+Write a function called repeatStr which repeats the given string string exactly n times.
+ */
+function repeatStr (n, s) {
+    return s.repeat([n]);
+}
+
+//====================================================================================
