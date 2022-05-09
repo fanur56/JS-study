183da41e51ae2c263b76252acadefc7a926cfaf3
fanur56 <ilasovf@gmail.com>
2022-05-09 18:39:29 +0300

    Add new solution

--- Solutions.js
+++ Solutions.js
@@ -150,3 +150,16 @@ function repeatStr (n, s) {
 }
 
 //====================================================================================
+/*
+Task:
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+ */
+var summation = function (num) {
+    // Code here
+    var sum = 0;
+    for (i = 0; i <= num; i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
+//=================================================================================
